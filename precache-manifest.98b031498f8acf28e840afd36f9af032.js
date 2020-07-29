f11f9e11693a75fdb69c207433f2a0c92383e552
Ryan Jacobson <ryan@Ryans-MacBook-Pro.local>
2020-07-28 23:49:52 -0400

    Updates

---
+++ precache-manifest.98b031498f8acf28e840afd36f9af032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6a1817d7ffe02ad160e1997efdc813",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "483eb690291ff619b869",
+    "url": "/giphy/static/css/main.81d55489.chunk.css"
+  },
+  {
+    "revision": "09cc3a1753cb7ed195de",
+    "url": "/giphy/static/js/2.16cd20be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy/static/js/2.16cd20be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483eb690291ff619b869",
+    "url": "/giphy/static/js/main.d1a5de14.chunk.js"
+  },
+  {
+    "revision": "b40e66dd66b453311767",
+    "url": "/giphy/static/js/runtime-main.fae907ec.js"
+  }
+]);
